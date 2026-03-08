91b7310ee8570bed04b340141379c980429a49bf
RedstoneThomas <111314572+RedstoneThomas@users.noreply.github.com>
2026-03-08 20:20:48 +0000

    Delete stardew modpack 2 directory

--- stardew modpack 2/[CP] Satoru Gojo NPC/Maps/SatoruSpouseRoom.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="SatoruSpouseRoom" tilewidth="512" tileheight="1088" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="32" height="32"/>
- <tile id="2165">
-  <image width="512" height="1088" source="SatoruSpouseRoom.png"/>
- </tile>
-</tileset>
--- stardew modpack 2/[CP] Satoru Gojo NPC/Maps/walls_and_floors.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="walls_and_floors" tilewidth="256" tileheight="688" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="32" height="32"/>
- <tile id="184">
-  <image width="256" height="688" source="walls_and_floors.png"/>
- </tile>
-</tileset>
